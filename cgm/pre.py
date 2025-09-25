236829af47e5e8b4d6058443e02de0c527606cb0
1rua11 <106394488+1rua11@users.noreply.github.com>
2025-09-25 23:34:05 +0800

    Add files via upload

---
+++ cgm/pre.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from misc import torchutils
+
+
+
+
+class Class_Predictor(nn.Module):
+    def __init__(self, num_classes, representation_size):
+        super(Class_Predictor, self).__init__()
+        self.num_classes = num_classes
+        self.classifier = nn.Conv2d(representation_size, num_classes, 1, bias=False)
+
+    def forward(self, x, label):
+        batch_size = x.shape[0]
+        x = x.reshape(batch_size,self.num_classes,-1)# bs*20*2048 .detach().clone()
+        mask = label>0 # bs*20
+
+        feature_list = [x[i][mask[i]] for i in range(batch_size)] # bs*n*2048
+        prediction = [self.classifier(y.unsqueeze(-1).unsqueeze(-1)).squeeze(-1).squeeze(-1) for y in feature_list]
+        labels = [torch.nonzero(label[i]).squeeze(1) for i in range(label.shape[0])]
+
+        loss = 0
+        acc = 0
+        num = 0
+        for logit,label in zip(prediction, labels):
+            if label.shape[0] == 0:
+                continue
+            loss_ce= F.cross_entropy(logit, label)
+            loss += loss_ce
+            acc += (logit.argmax(dim=1)==label.view(-1)).sum().float()
+            num += label.size(0)
+            
+        return loss/batch_size, acc/num
